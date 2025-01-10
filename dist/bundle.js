30f7b9ef3a0615581bc65d099223f79ec2840659
Aquasonic1995 <Aquasonic1995@gmail.com>
2025-01-10 18:48:17 +0300

    Add pre-commit hook

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,27 +6,30 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/index.js":
-/*!**********************!*\
+/******/ (() => {
+  // webpackBootstrap
+  /******/ var __webpack_modules__ = {
+    /***/ "./src/index.js":
+      /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+      /***/ () => {
+        eval(
+          'function addNumbers() {\r\n    const num1 = parseFloat(document.getElementById("number1").value);\r\n    const num2 = parseFloat(document.getElementById("number2").value);\r\n    const result = num1 + num2;\r\n    document.getElementById("result").innerText = `Result: ${result}`;\r\n}\r\n\r\ndocument.getElementById("calculate").addEventListener("click", addNumbers);\n\n//# sourceURL=webpack://innowiselabinternship/./src/index.js?',
+        );
 
-eval("function addNumbers() {\r\n    const num1 = parseFloat(document.getElementById(\"number1\").value);\r\n    const num2 = parseFloat(document.getElementById(\"number2\").value);\r\n    const result = num1 + num2;\r\n    document.getElementById(\"result\").innerText = `Result: ${result}`;\r\n}\r\n\r\ndocument.getElementById(\"calculate\").addEventListener(\"click\", addNumbers);\n\n//# sourceURL=webpack://innowiselabinternship/./src/index.js?");
+        /***/
+      },
 
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
-/******/ 	
-/******/ })()
-;
+    /******/
+  };
+  /************************************************************************/
+  /******/
+  /******/ // startup
+  /******/ // Load entry module and return exports
+  /******/ // This entry module can't be inlined because the eval devtool is used.
+  /******/ var __webpack_exports__ = {};
+  /******/ __webpack_modules__["./src/index.js"]();
+  /******/
+  /******/
+})();
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,9 @@ import pluginJs from "@eslint/js";
 import prettierPlugin from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
 
+
 /** @type {import('eslint').Linter.Config[]} */
+import "eslint-plugin-only-warn";
 export default [
   {
     files: ["**/*.js"], // Apply to all .js files
@@ -17,6 +19,14 @@ export default [
     rules: {
       ...prettierConfig.rules, // Include Prettier rules to disable ESLint conflicts
       "prettier/prettier": "error", // Enable Prettier as a rule
+      "no-unused-vars": [
+        "warn",
+        {
+          varsIgnorePattern: "__webpack_exports__", // Ignore variables matching this pattern
+          args: "none", // Do not check function arguments
+        },
+      ],
+
     },
   },
   pluginJs.configs.recommended, // Use recommended rules from @eslint/js

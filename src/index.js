7230aac87634256a6a65a7cadea536a5512adad2
Aquasonic1995 <Aquasonic1995@gmail.com>
2025-01-13 03:51:41 +0300

    Add display value logic and clear function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-require('styles.css');
+require('./styles.css');
 if (module.hot) {
   module.hot.accept(); // Accept updates for hot-reloaded modules
 }
 const body = document.body;
-
+const display = document.getElementById('display');
+const clear = document.getElementById('clear');
 const orangeButton = document.getElementById('orangeButton');
 const purpleButton = document.getElementById('purpleButton');
 const colorButton = document.getElementById('colorButton');
@@ -22,3 +23,22 @@ purpleButton.addEventListener('click', () => {
 colorButton.addEventListener('click', () => {
   body.className = 'color-theme';
 });
+
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const value = button.textContent; // Get the value from the button
+    addToDisplay(value); // Call addToDisplay with the button's value
+    console.log(value);
+  });
+});
+function addToDisplay(input) {
+  display.value += input; // Append input to the display
+}
+clear.addEventListener('click', () => {
+  clearDisplay(); // Call clearDisplay when clicked
+});
+
+function clearDisplay() {
+  display.value = '';
+}
